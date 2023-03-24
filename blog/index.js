a3e498a5be333798919ae3af2a87b5c44d7c47d1
Umedjon0005 <97680790+Umedjon0005@users.noreply.github.com>
2023-03-24 11:43:45 +0500

    Add files via upload

--- blog/index.js
+++ blog/index.js
@@ -1,4 +1,5 @@
 import express  from "express";
+import fs from 'fs';
 import multer from "multer";
 import mongoose from "mongoose";
 import { registerValidation, loginValidation,  postCreateValidation} from "./validations.js";
@@ -14,6 +15,9 @@ const app = express();
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        if(!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads')
+        }
         cb(null, 'uploads')
     },
     filename: (_, file, cb) => {
